c551cd52a7491ced81aa62b4cebfe08d338270df
sujatamadke <sujatamadke1995@gmail.com>
2020-09-03 15:17:24 +0530

    quiz content added in js format for natural circulation

---
+++ exp1/Quiz_js_format.js
@@ -0,0 +1,159 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title> Quiz </title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/adi.css">
+
+  </head>
+  <body>
+    <div class="container  ce">
+        <div class="container">
+          <div class="jumbotron ce">
+            <header>
+              <div class="row ce ">
+                <div class="col-sm-3 ">
+                  <img src="./images/vlabs.png" class="p1" alt="VLabs Logo">
+                </div>
+                <div class="col-sm-6  tex">
+                  <p><strong> Virtual Labs <br>
+                    IIT Kharagpur</strong></p>
+                </div>
+                <div class="col-sm-3 ">
+                  <img src="./images/iitkgp.png" class="p2"  alt="IIT Khargpur Logo">
+                </div>
+              </div>
+            </header>
+          </div>
+        </div>
+        <div class="container  ce">
+
+          <div class="container" id="ti">
+            <div class="row q ce">
+              <div class="col-sm-12">
+                <h2>Start Quiz</h2> <br>
+                <button type="button" class=" ce" id="st"><strong>Start</strong></button>
+              </div>
+            </div>
+          </div>
+
+          <div class="container" id="qes">
+            <div class="row q" id="Qs">
+                <div class="col-sm-12">
+                    <h2>Quiz</h2>
+                    <br>
+                    <h3 id="Qe"></h3>
+                    <br>
+                    <div class="row q lf" id="q1">
+                      <div class="col-sm-12">
+                        <p>
+                          Question 1 	In which of the following internal wick structure is present?
+                          <br>
+                          <form method="post" id="f1">
+                          <p class="an">  <input type="radio" name="Aditya" value="q1" id="q11"> &nbsp;<label for="Aditya"> Q1</label></p>
+                          <p class="an">  <input type="radio" name="Aditya" value="q1" id="q12"> &nbsp;<label for="Aditya"> Q2</label></p>
+                          <p class="an">  <input type="radio" name="Aditya" value="q1" id="q13">  &nbsp;<label for="Aditya"> Q3</label></p>
+                          <p class="an">  <input type="radio" name="Aditya" value="q1" id="q14">  &nbsp;<label for="Aditya"> Q4</label></p>
+                          </form>
+                        </p>
+                      </div>
+                    </div>
+                    <br>
+                    <div class="row q lf" id="q2">
+                      <div class="col-sm-12">
+                        <p>
+                          Question 2 <br>
+                          <form method="post" id="f2">
+                              <p class="an">  <input type="radio" name="Aditya" value="q2" id="q21"> &nbsp;<label for="Aditya"> Q1</label></p>
+                              <p class="an">  <input type="radio" name="Aditya" value="q2" id="q22">
+                            &nbsp;<label for="Aditya"> Q2</label></p>
+                              <p class="an">  <input type="radio" name="Aditya" value="q2" id="q23">
+                            &nbsp;<label for="Aditya"> Q3</label></p>
+                              <p class="an">  <input type="radio" name="Aditya" value="q2" id="q24">
+                            &nbsp;<label for="Aditya"> Q4</label></p>
+                          </form>
+                        </p>
+                      </div>
+                    </div>
+                    <br>
+                    <div class="row q lf" id="q3">
+                      <div class="col-sm-12">
+                        <p>
+                          Question 3 <br>
+                          <form method="post" id="f3">
+                              <p class="an">  <input type="radio" name="Aditya" value="q3" id="q31"> &nbsp;<label for="Aditya"> Q1</label></p>
+                              <p class="an">  <input type="radio" name="Aditya" value="q3" id="q32">
+                            &nbsp;<label for="Aditya"> Q2</label></p>
+                              <p class="an">  <input type="radio" name="Aditya" value="q3" id="q33">
+                            &nbsp;<label for="Aditya"> Q3</label></p>
+                              <p class="an">  <input type="radio" name="Aditya" value="q3" id="q34">
+                            &nbsp;<label for="Aditya"> Q4</label></p>
+                          </form>
+                        </p>
+                      </div>
+                    </div>
+                    <br>
+                    <div class="row q lf" id="q4">
+                      <div class="col-sm-12">
+                        <p>
+                          Question 4 <br>
+                          <form method="post" id="f4">
+                              <p class="an">  <input type="radio" name="Aditya" value="q4" id="q41"> &nbsp;<label for="Aditya"> Q1</label></p>
+                              <p class="an">  <input type="radio" name="Aditya" value="q4" id="q42">
+                            &nbsp;<label for="Aditya"> Q2</label></p>
+                              <p class="an">  <input type="radio" name="Aditya" value="q4" id="q43">
+                            &nbsp;<label for="Aditya"> Q3</label></p>
+                              <p class="an">  <input type="radio" name="Aditya" value="q4" id="q44">
+                            &nbsp;<label for="Aditya"> Q4</label></p>
+                          </form>
+                        </p>
+                      </div>
+                    </div>
+                    <br>
+                </div>
+            </div>
+            <div class="row q ce" id="rst">
+              <div class="col-sm-12">
+                <h2>Result</h2> <br>
+                <h3 id="ma">Result</h3> <br>
+                <button type="reset" value="Reset" class=" ce" id="rst"><strong>Re-Start</strong></button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      <div class="container ce">
+        <div class="row">
+          <div class="col-sm-12">
+            &nbsp;
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="jumbotron  ce">
+          <footer>
+            <div class="row ce">
+              <div class="col-sm-12">
+                <p><i> Copyright &copy;2020 Virtual labs IIT Kharagpur</i></p>
+              </div>
+            </div>
+          </footer>
+        </div>
+      </div>
+  </div>
+  <script src="./js/jquery.slim.min.js"></script>
+  <script src="./js/popper.min.js"></script>
+  <script src="./js/bootstrap.min.js"></script>
+  <script src="./js/adi.js"></script>
+
+  <!--
+  This Scrtpt file is developed by
+  Aditya Kameswara Rao Nandula
+  Senior Research Fellow,
+  Virtual Labs IIT Kharagpur.
+  Email: akraonandula@gmail.com
+
+  -->
+  </body>
