f7a00540172f98bc048dff85866fdc33def5c117
sujatamadke <sujatamadke1995@gmail.com>
2020-12-16 16:19:42 +0530

    Metacentric_expt simulation added

---
+++ js/Metacentric_expt.js
@@ -0,0 +1,8 @@
+﻿function multiplyBy()
+{
+        num1 = document.getElementById("weight1").value;
+        num2 = document.getElementById("weight2").value;
+		num3 = document.getElementById("distance").value;
+		num4 = document.getElementById("angle").value;
+        document.getElementById("result").innerHTML =(num2 * num3)/((num1+num2)* Math.tan(num4));
+}
