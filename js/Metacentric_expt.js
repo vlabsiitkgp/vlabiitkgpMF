71efac6f2957ca86cda5f8fbe86797b30cf8451c
Sujata Madke <sujatamadke1995@gmail.com>
2021-10-11 15:57:21 +0530

    abc

--- js/Metacentric_expt.js
+++ js/Metacentric_expt.js
@@ -4,5 +4,5 @@
         num2 = document.getElementById("weight2").value;
 		num3 = document.getElementById("distance").value;
 		num4 = document.getElementById("angle").value;
-        document.getElementById("result").innerHTML =(num2 * num3)/((num1+num2)* Math.tan(num4 * Math.PI / 180));
+        document.getElementById("result").innerHTML =(num2 * num3 * 100)/((num1+num2)* Math.tan(num4));
 }
